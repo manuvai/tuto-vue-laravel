8d9fff7fc4a0d0119e849894f31c92a31ff70107
manuvai <rm90266@gmail.com>
2021-06-12 13:30:42 -1000

    Episode 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,25 @@
     </head>
     <body>
         <div id="app">
-            <div class="container pt-3">
-                <example-component></example-component>
-
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <router-link to="/home" class="nav-link">Accueil</router-link>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <router-link to="/task" class="nav-link">Liste des tâches</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="container">
+                <router-view></router-view>
             </div>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
