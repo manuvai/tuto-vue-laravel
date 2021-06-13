84713aff58d2a265c6608a457b85982b1eec33bb
manuvai <rm90266@gmail.com>
2021-06-12 14:26:19 -1000

    Episode 5

--- public/js/app.js
+++ public/js/app.js
@@ -1941,8 +1941,20 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
 /* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      tasks: {}
+    };
+  },
+  created: function created() {
+    var _this = this;
+
+    axios.get('/taskslist').then(function (response) {
+      _this.tasks = response.data;
+      console.log(response);
+    })["catch"]();
+  },
   mounted: function mounted() {
     console.log('Component mounted.');
   }
@@ -37644,34 +37656,20 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _vm._m(0)
-}
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "container" }, [
-      _c("ul", { staticClass: "list-group" }, [
-        _c("li", { staticClass: "list-group-item" }, [
-          _c("a", { attrs: { href: "#" } }, [_vm._v("Tâche 1")])
-        ]),
-        _vm._v(" "),
-        _c("li", { staticClass: "list-group-item" }, [
-          _c("a", { attrs: { href: "#" } }, [_vm._v("Tâche 2")])
-        ]),
-        _vm._v(" "),
-        _c("li", { staticClass: "list-group-item" }, [
-          _c("a", { attrs: { href: "#" } }, [_vm._v("Tâche 3")])
-        ]),
-        _vm._v(" "),
-        _c("li", { staticClass: "list-group-item" }, [
-          _c("a", { attrs: { href: "#" } }, [_vm._v("Tâche 4")])
+  return _c("div", { staticClass: "container" }, [
+    _c(
+      "ul",
+      { staticClass: "list-group" },
+      _vm._l(_vm.tasks, function(task) {
+        return _c("li", { key: task.id, staticClass: "list-group-item" }, [
+          _c("a", { attrs: { href: "#" } }, [_vm._v(_vm._s(task.name))])
         ])
-      ])
-    ])
-  }
-]
+      }),
+      0
+    )
+  ])
+}
+var staticRenderFns = []
 render._withStripped = true
 
 
--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="container">
         <ul class="list-group">
-            <li class="list-group-item"><a href="#">Tâche 1</a></li>
-            <li class="list-group-item"><a href="#">Tâche 2</a></li>
-            <li class="list-group-item"><a href="#">Tâche 3</a></li>
-            <li class="list-group-item"><a href="#">Tâche 4</a></li>
+            <li class="list-group-item" v-for="task in tasks" :key="task.id">
+                <a href="#">{{ task.name }}</a>
+            </li>
         </ul>
     </div> 
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                tasks: {}
+            }
+        },
+        created() {
+            axios.get('/taskslist')
+                .then(response => {
+                    this.tasks = response.data
+                    console.log(response)
+                })
+                .catch()
+        },
+
         mounted() {
             console.log('Component mounted.')
         }
